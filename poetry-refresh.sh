c3d6424c645c3539d132781c4d350bb3d1dbcb5a
Yin-Chi Chan <ycc39@cam.ac.uk>
2024-06-06 16:12:03 +0100

    Update poetry-refresh.sh

--- poetry-refresh.sh
+++ poetry-refresh.sh
@@ -6,9 +6,15 @@ poetry install
 cd ..
 echo -e '\n\n'
 
+# Any directory equal to or starting with "./digital-hospitals-docs"
+find . -maxdepth 1 -regex '\./digital-hospitals-docs.*' | \
+xargs -I % bash -c 'cd %; pwd; poetry lock; poetry install; cd ..; echo -e "\n\n"'
+
+# Any directory starting with "./digital-hospitals." except "digital-hospitals.common"
 find . -maxdepth 1 -regex '\./digital-hospitals\..+' -not -name 'digital-hospitals.common' | \
 xargs -I % bash -c 'cd %; pwd; poetry lock; poetry install; cd ..; echo -e "\n\n"'
 
+# Master "digital-hospitals" directory
 cd digital-hospitals
 pwd
 poetry lock
